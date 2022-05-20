2e7d52f7edfc41ae52307a7dc77b7a4ec5c3f238
ZakirAvrora <krasavasula@gmail.com>
2022-05-20 18:59:36 +0600

    version 1.0

--- cache.go
+++ cache.go
@@ -1,23 +1,59 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Pair struct {
+	key      string
+	value    string
+	deadline time.Time
+}
 
 type Cache struct {
+	Dict []Pair
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cash *Cache) Get(key string) (string, bool) {
+	for _, pair := range cash.Dict {
+		if pair.key == key && pair.deadline.IsZero() {
+			return pair.value, true
+		} else if pair.key == key && time.Now().Before(pair.deadline) {
+			return pair.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (cash *Cache) Put(key, value string) {
+	for i, pair := range cash.Dict {
+		if pair.key == key {
+
+			cash.Dict[i].value = value
+			return
+		}
+	}
+	newPair := Pair{key, value, time.Time{}}
+	cash.Dict = append(cash.Dict, newPair)
 }
 
-func (receiver) Keys() []string {
+func (cash *Cache) Keys() []string {
+	var keys []string
+	for _, pair := range cash.Dict {
+		if pair.deadline.IsZero() {
+			keys = append(keys, pair.key)
+		} else if time.Now().Before(pair.deadline) {
+			keys = append(keys, pair.key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cash *Cache) PutTill(key, value string, deadline time.Time) {
+	newPair := Pair{key, value, deadline}
+	cash.Dict = append(cash.Dict, newPair)
 }
